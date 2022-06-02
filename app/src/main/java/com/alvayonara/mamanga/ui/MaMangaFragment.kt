5441066727e480166953a6748a2d7543b72188d6
Alva Yonara Puramandya <alvayonara@outlook.com>
2022-06-03 04:23:00 +0700

    feat(bottom nav): init fragment

---
+++ app/src/main/java/com/alvayonara/mamanga/ui/MaMangaFragment.kt
@@ -0,0 +1,18 @@
+package com.alvayonara.mamanga.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.alvayonara.mamanga.R
+
+class MaMangaFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_mamanga, container, false)
+    }
+}
